f604e51ee020294d3346915771c1606248eb71cb
yefelso <131312@senati.pe>
2023-03-23 11:26:04 -0500

    sumadenumerospares_while

---
+++ Proyect4.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyect.pkg4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Proyect4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        int numero = 0;
+        Scanner sm = new Scanner (System.in);
+        System.out.println("ingrese un numero");
+        numero = sm.nextInt();
+        int suma=0;
+        int i = 0;
+        while ( i <= numero){
+        suma = suma + i;
+        i = i+2;
+        
+      
+    }
+        System.out.println("la suma de los numeros pares es :"+ suma);
+}
+}
